1c13ef8d267cb12ef0d92744a6f26f623029ee7c
Michael Ledin <mledin89@gmail.com>
2019-08-02 14:37:08 +0300

    v1.0.0
    Update docs

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # laravel-queue-rate-limit
 Simple Laravel queue rate limiting
+
+## Installation
+```bash
+$ composer require mxl/laravel-queue-rate-limit:^1.0
+```
+
+Laravel 5.5+ will use the [auto-discovery](https://medium.com/@taylorotwell/package-auto-discovery-in-laravel-5-5-ea9e3ab20518) feature to add `MichaelLedin\LaravelQueueRateLimit\QueueServiceProvider::class` to providers.
+
+This package is not compatible with older Laravel versions.
+
+Add rate limits to `config/queue.php`:
+
+```php
+'rateLimits' => [
+     'mail' => [ // queue name
+        'allows' => 1, // 1 job
+        'every' => 5 // per 5 seconds
+     ]
+]
+```
+
+## Usage
+
+Make sure that you don't use `sync` connection when queueing jobs. See `default` property in `config/queue.php`.
+
+Run queue worker:
+
+```bash
+$ php artisan queue:work --queue default,mail
+```
+
+Then push several jobs to `default` and `mail` queues:
+
+```php
+Mail::queue(..., 'mail');
+Mail::queue(..., 'mail');
+Mail::queue(..., 'mail');
+Mail::queue(..., 'default');
+Mail::queue(..., 'default');
+```
+
+You'll see that only `mail` queue jobs will be rate limited while `default` queue jobs will run normally.
+
+## Maintainers
+
+- [@mxl](https://github.com/mxl)
+
+## License
+
+See the [LICENSE](https://github.com/mxl/laravel-queue-rate-limit/blob/master/LICENSE) file for details.
+
+
---
+++ src/Worker.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace MichaelLedin\LaravelQueueRateLimit;
+
+use Illuminate\Cache\RateLimiter;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Log\LogManager;
+use Illuminate\Queue\QueueManager;
+
+class Worker extends \Illuminate\Queue\Worker
+{
+    /**
+     * @var array
+     */
+    private $rateLimits;
+
+    /**
+     * @var RateLimiter
+     */
+    private $rateLimiter;
+
+    /**
+     * @var LogManager
+     */
+    private $logger;
+
+    /**
+     * @inheritDoc
+     * @param array|null $rateLimits
+     * @param RateLimiter $rateLimiter
+     * @param LogManager $logger
+     */
+    public function __construct(QueueManager $manager, Dispatcher $events, ExceptionHandler $exceptions, $rateLimits, $rateLimiter, $logger)
+    {
+        parent::__construct($manager, $events, $exceptions);
+
+        $this->rateLimits = $rateLimits ?? [];
+        $this->rateLimiter = $rateLimiter;
+        $this->logger = $logger;
+    }
+
+    protected function getNextJob($connection, $queue)
+    {
+        $job = null;
+        foreach (explode(',', $queue) as $queue) {
+            $rateLimit = $this->rateLimits[$queue] ?? null;
+            if ($rateLimit) {
+                if (!isset($rateLimit['allows']) || !isset($rateLimit['every'])) {
+                    throw new \RuntimeException('Set "allows" and "every" fields for "' . $queue . '" rate limit.');
+                }
+                $this->log('Rate limit is set for queue ' . $queue);
+                if ($this->rateLimiter->tooManyAttempts($queue, $rateLimit['allows'])) {
+                    $availableIn = $this->rateLimiter->availableIn($queue);
+                    $this->log('Rate limit is reached for queue ' . $queue . '. Next job will be started in ' . $availableIn . ' seconds');
+                    continue;
+                } else {
+                    $this->log('Rate limit check is passed for queue ' . $queue);
+                }
+            } else {
+                $this->log('No rate limit is set for queue ' . $queue . '.');
+            }
+
+            $job = parent::getNextJob($connection, $queue);
+            if ($job) {
+                if ($rateLimit) {
+                    $this->rateLimiter->hit($queue, $rateLimit['every']);
+                }
+                $this->log('Running job ' . $job->getJobId() . ' on queue ' . $queue);
+                break;
+            } else {
+                $this->log('No available jobs on queue ' . $queue);
+            }
+        }
+        return $job;
+    }
+
+
+    private function log(string $message)
+    {
+        if ($this->logger) {
+            $this->logger->debug($message);
+        }
+    }
+}
