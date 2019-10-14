e0c2994fc0b27c6f3c5cfd186e72f0b2e8e71cf2
Martin Hettiger <martin@hettiger.com>
2019-10-14 14:03:43 +0200

    fix(service-provider): pass all required params to the constructor

--- src/QueueServiceProvider.php
+++ src/QueueServiceProvider.php
@@ -11,9 +11,18 @@ class QueueServiceProvider extends \Illuminate\Queue\QueueServiceProvider
     protected function registerWorker()
     {
         $this->app->singleton('queue.worker', function () {
+            $isDownForMaintenance = function () {
+                return $this->app->isDownForMaintenance();
+            };
+
             return new Worker(
-                $this->app['queue'], $this->app['events'], $this->app[ExceptionHandler::class],
-                $this->app['config']->get('queue.rateLimits'), $this->app[RateLimiter::class], $this->app[LogManager::class]
+                $this->app['queue'],
+                $this->app['events'],
+                $this->app[ExceptionHandler::class],
+                $isDownForMaintenance,
+                $this->app['config']->get('queue.rateLimits'),
+                $this->app[RateLimiter::class],
+                $this->app[LogManager::class]
             );
         });
     }
